53bd4fc8ab1bf5672cfa4e4783b65e8e011022f8
HuGang <hugang@bailongma-inc.com>
2020-07-31 17:02:41 +0800

    依赖更新

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 /*
- * @Description: 
+ * @Description: 主模块
  * @Author: HuGang
  * @Date: 2019-12-02 13:28:14
- * @LastEditTime: 2020-07-25 13:36:06
+ * @LastEditTime: 2020-07-31 15:04:39
  */
+// 依赖库
 const Koa = require('koa')
-const catchError = require('./middlewares/catcherror');
-const sequelize = require('./lib/dbConn');
-const router = require('./router/router')
 const cors = require('@koa/cors') // 用于开启跨域
 const bodyParser = require('koa-bodyparser') // 请求体解析中间件
+// 配置
+const sequelize = require('./database/dbConn'); // 数据库
+const catchError = require('./middlewares/catcherror'); // 全局错误处理
+const router = require('./router') // 路由
 
 const app = new Koa()
+
 // 注册中间件
 app
   .use(catchError)
@@ -24,12 +27,13 @@ app.use((ctx) => {
   console.log("404 Not Found")
 });
 
+// 数据库建连
 sequelize.authenticate().then(() => {
-  console.log('Success.');
+  console.log('*****数据库连接成功，─=≡Σ(((つ•̀ω•́)つ*****');
 }).catch(err => {
   console.error('Failed', err);
 });
 
+// 监听端口启动http服务
 app.listen(3444)
-
-console.log(`listening on port 3444`)
+console.log(`*****项目启动成功，m(=∩王∩=)m*****`)
