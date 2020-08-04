f9c1b2ab44fc0c7fddcbb839bb410d24af0a9b32
胡小呆 <416681736@qq.com>
2020-08-05 00:12:28 +0800

    user API

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,24 +2,23 @@
  * @Description: 
  * @Author: HuGang
  * @Date: 2020-07-16 16:56:11
- * @LastEditTime: 2020-07-31 16:58:12
+ * @LastEditTime: 2020-08-05 00:12:02
  */ 
-// constroller 用于处理用户输入
-const user = require('../service/UserService');
+const UserService = require('../service/UserService');
 
-const checkLogin = async (ctx, next) => {
-  const { phone, password } = ctx.request.body
-  const data = await user.checkUser(phone, password)
+const userLogin = async (ctx, next) => {
+  const { user, password } = ctx.request.body
+  const data = await UserService.SQLuserLogin(user, password)
   return ctx.response.body = data
 } 
 
-const registerUser = async (ctx, next) => {
-  const { phone, password } = ctx.request.body
-  const data = await user.createUser(phone, password)
+const userRegister = async (ctx, next) => {
+  const { user, password } = ctx.request.body
+  const data = await UserService.SQLuserRegister(user, password)
   return ctx.response.body = data
 } 
 
 module.exports = {
-  checkLogin,
-  registerUser
+  userLogin,
+  userRegister
 }
--- src/router/userRoute.js
+++ src/router/userRoute.js
@@ -2,13 +2,13 @@
  * @Description: 
  * @Author: HuGang
  * @Date: 2020-07-16 16:52:29
- * @LastEditTime: 2020-08-04 22:26:08
+ * @LastEditTime: 2020-08-05 00:11:30
  */ 
 const Router = require('@koa/router')
 const controller = require('../controller/userController')
 
 const router = new Router()
-router.post('/login', controller.checkLogin)
-router.post('/register', controller.registerUser)
+router.post('/login', controller.userLogin)
+router.post('/register', controller.userRegister)
 
 module.exports = router
