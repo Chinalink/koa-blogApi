131a046eab9b29211d4c10519d6e033ed4a076a0
胡小呆 <416681736@qq.com>
2020-08-09 19:33:00 +0800

    用户权限判断

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,13 +2,20 @@
  * @Description: 
  * @Author: HuGang
  * @Date: 2020-08-01 16:47:45
- * @LastEditTime: 2020-08-05 23:28:12
+ * @LastEditTime: 2020-08-09 19:18:10
  */ 
 
 class Validation {
+  // 是否为空
   static isEmpty(value, msg) {
     if (!value || value === '') {
-      throw global.ParameterException(msg)
+      throw new global.ParameterException(msg)
+    }
+  }
+
+  static isSelf(uid, targetId) {
+    if (uid !== 1 && uid != targetId) {
+      throw new global.AuthFaild('权限不足', 1003)
     }
   }
 }
