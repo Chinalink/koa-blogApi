4b625bcfb8f24bb485d64bdd2901eeba421d373a
HuXiaoDai <416681736@qq.com>
2021-05-25 15:42:47 +0800

    注册路由中间件、校验工具

--- src/core/index.js
+++ src/core/index.js
@@ -2,14 +2,26 @@
  * @Description: 中间件配置及加载
  * @Author: HuGang
  * @Date: 2021-05-24 16:27:56
- * @LastEditTime: 2021-05-24 17:11:08
+ * @LastEditTime: 2021-05-25 15:39:44
  */
 const sequelize = require('./db'); // 数据库ORM
+
+const Router = require('@koa/router') // 路由中间件
+const requireDirectory = require('require-directory'); // 路由自动注册
+
+const validator = require('validator'); // 表单校验
+
 class InitManager {
   // 初始化
   static initCore(app) {
     InitManager.app = app
     InitManager.initLoadSequelize()
+    InitManager.initLoadRouters()
+    InitManager.initLoadValidator()
+  }
+
+  static initLoadValidator() {
+    global.validator = validator
   }
 
   // 数据库连接
@@ -21,6 +33,22 @@ class InitManager {
       console.error('数据库连接失败', error)
     }
   }
+
+  // 初始化路由
+  static initLoadRouters() {
+    const router = new Router({ prefix: '/apis/v1' }) // 路由实例
+    const apiDirectory = `${process.cwd()}/src/routes` // process.cwd() 获取程序根目录
+
+    requireDirectory(module, apiDirectory, {
+      visit: r => {
+        if (r instanceof Router) {
+          router.use(r.routes())
+        }
+      }
+    })
+    InitManager.app.use(router.routes())
+  }
+
 }
 
 module.exports = InitManager
